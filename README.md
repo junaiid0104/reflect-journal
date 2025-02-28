cd48bce86ed354cb0dac99251d61d955ba8dc1e4
Junaid Mohd <junaiid0104@gmail.com>
2025-02-28 20:38:23 +0530

    🚀 Preparing for deployment: Finalizing configurations and optimizations

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
+<<<<<<< HEAD
 # reflect-journal
 Reflect - A Modern Digital Journal 📝✨ A personal journaling app with insightful prompts, mood analytics, and seamless organization. Built with Next.js, Shadcn UI, Prisma, and PostgreSQL. 🚀
+=======
+# Full Stack Journal App with Next JS, React, Tailwind CSS, Prisma, Neon, Clerk, ArcJet, Shadcn UI Tutorial 🔥🔥
+## https://youtu.be/cYGAPVUtQ-U
+
+![image](https://github.com/user-attachments/assets/8e4fbd95-40af-4496-a319-dd74d3954814)
+
+### Make sure to create a `.env` file with following variables -
+
+```
+NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY=
+CLERK_SECRET_KEY=
+
+NEXT_PUBLIC_CLERK_SIGN_IN_URL=/sign-in
+NEXT_PUBLIC_CLERK_SIGN_UP_URL=/sign-up
+NEXT_PUBLIC_CLERK_AFTER_SIGN_IN_URL=/dashboard
+NEXT_PUBLIC_CLERK_AFTER_SIGN_UP_URL=/dashboard
+
+DATABASE_URL=
+
+PIXABAY_API_KEY=
+
+ARCJET_KEY=
+```
+>>>>>>> 4ed9897 (🚀 Preparing for deployment: Finalizing configurations and optimizations)
---
+++ app/layout.js
@@ -0,0 +1,53 @@
+import Header from "@/components/header";
+import "./globals.css";
+import { Inter } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "sonner";
+import "react-quill-new/dist/quill.snow.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Reflect 🔁📔",
+  description:
+    "Reflect 🔁📔 is a powerful journaling app designed to help you capture your thoughts, track personal growth, and reflect on past experiences. With an intuitive interface, seamless cloud sync, and smart insights, Reflect makes it easy to build a consistent journaling habit. Whether you're jotting down daily notes, setting goals, or revisiting past reflections, this app empowers you to stay mindful and organized.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <ClerkProvider
+    // appearance={{
+    //   baseTheme: shadesOfPurple,
+    //   variables: {
+    //     colorPrimary: "#3b82f6",
+    //     colorBackground: "#1a202c",
+    //     colorInputBackground: "#2D3748",
+    //     colorInputText: "#F3F4F6",
+    //   },
+    //   elements: {
+    //     formButtonPrimary: "bg-purple-600 hover:bg-purple-700 text-white",
+    //     card: "bg-gray-800",
+    //     headerTitle: "text-blue-400",
+    //     headerSubtitle: "text-gray-400",
+    //   },
+    // }}
+    >
+      <html lang="en">
+        <body
+          className={`${inter.className} bg-gradient-to-b from-orange-50 via-amber-50 to-orange-50`}
+        >
+          <div className="inset-0 bg-[url('/bg.jpg')] opacity-50 fixed -z-10" />
+          <Header />
+          <main className="min-h-screen">{children}</main>
+          <Toaster richColors />
+
+          <footer className="bg-orange-300 py-12 bg-opacity-10">
+            <div className="container mx-auto px-4 text-center text-gray-900">
+              <p>Made with 🖤 by Junaid</p>
+            </div>
+          </footer>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
